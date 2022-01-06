ec49f2ac926e613c21af0d40c8b5de267db99d0d
ddosnikgit <ddosnikofficial@gmail.com>
2022-01-06 17:35:06 +0300

    Update

---
+++ src/Main.php
@@ -0,0 +1,56 @@
+<?php
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
+  protected $isFlooding = false;
+  protected $handleQueue = [];
+  public function onLoad() 
+  {
+    \pocketmine\Server::getInstance()->getLogger()->notice('PocketShield plugin successfully started!');
+    \pocketmine\Server::getInstance()->getLogger()->notice('GIT - https://github.com/ddosnikgit/PocketShield');
+  }
+  public function onEnable()
+  {
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function isFloodingPackets($ip, $port){
+		if(!empty($ip) and !empty($port)){
+			if(!isset($this->handleQueue[$ip][$port])){
+			    $this->handleQueue[$ip][$port] = [
+			        'time' => time(),
+			        'packets' => 0
+			    ];
+		    }
+
+		    if ($this->handleQueue[$ip][$port]['time'] < time()) {
+		    	unset($this->handleQueue[$ip][$port]);
+
+		    	return false;
+		    }
+
+		    if(time() == $this->handleQueue[$ip][$port]['time']){
+		    	$this->handleQueue[$ip][$port]['packets']++;
+		    }
+
+		    if($this->handleQueue[$ip][$port]['packets'] > 450){
+		    	$this->isFlooding = true;
+		    	
+			    \pocketmine\Server::getInstance()->getLogger()->warning("Flood detected to user-ip: {$ip}");
+			    \pocketmine\Server::getInstance()->getLogger()->warning("PacketsPerSecond: {$this->handleQueue[$ip][$port]['packets']}");
+
+			    //$this->getServer()->getNetwork()->blockAddress($ip);
+			    unset($this->handleQueue[$ip]);
+
+			    return true;
+		    }
+		    
+		    return false;
+		}
+	}
+  public function MethodFlood(\pocketmine\event\server\DataPacketReceiveEvent $pk) {
+    $flood = $this->isFloodingPackets($pk->getPlayer()->getAddress(), $pk->getPlayer()->getPort());
+       if($flood == true){
+         $pk->getPlayer()->close();
+		 $this->getServer()->getNetwork()->blockAddress($pk->getPlayer()->getAddress(), 700);
+         return;
+       }
+  }
+}
